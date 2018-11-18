aed934bbc323e88175538f95463c6d09684f8a5e
Unknown <Danil.Koporushkin@urfu.me>
2018-11-18 14:34:42 +0500

    fixs for video chats

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,68 +21,7 @@ const emoji = randomEmoji();
 const drone = new ScaleDrone('pMbakAnQeahIRBtF');
 const roomName = 'observable-' + roomHash;
 const configuration = {
-    iceServers: [{
-        url: 'stun:stun01.sipphone.com'
-    },
-        {
-            url: 'stun:stun.ekiga.net'
-        },
-        {
-            url: 'stun:stun.fwdnet.net'
-        },
-        {
-            url: 'stun:stun.ideasip.com'
-        },
-        {
-            url: 'stun:stun.iptel.org'
-        },
-        {
-            url: 'stun:stun.rixtelecom.se'
-        },
-        {
-            url: 'stun:stun.schlund.de'
-        },
-        {
-            url: 'stun:stun.l.google.com:19302'
-        },
-        {
-            url: 'stun:stun1.l.google.com:19302'
-        },
-        {
-            url: 'stun:stun2.l.google.com:19302'
-        },
-        {
-            url: 'stun:stun3.l.google.com:19302'
-        },
-        {
-            url: 'stun:stun4.l.google.com:19302'
-        },
-        {
-            url: 'stun:stunserver.org'
-        },
-        {
-            url: 'stun:stun.softjoys.com'
-        },
-        {
-            url: 'stun:stun.voiparound.com'
-        },
-        {
-            url: 'stun:stun.voipbuster.com'
-        },
-        {
-            url: 'stun:stun.voipstunt.com'
-        },
-        {
-            url: 'stun:stun.voxgratia.org'
-        },
-        {
-            url: 'stun:stun.xten.com'
-        },
-        {
-            url: 'turn:numb.viagenie.ca',
-            credential: 'muazkh',
-            username: 'webrtc@live.com'
-        },
+    iceServers: [
         {
             url: 'turn:192.158.29.39:3478?transport=udp',
             credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
@@ -130,7 +69,6 @@ function sendMessage(message) {
 }
 
 function startWebRTC(isOfferer) {
-    console.info('sendmessage')
 
     pc = new RTCPeerConnection(configuration);
 
@@ -148,11 +86,13 @@ function startWebRTC(isOfferer) {
             pc.createOffer(localDescCreated, error => console.error(error));
         };
         dataChannel = pc.createDataChannel('chat');
+        setupDataChannel();
+
     } else {
         pc.ondatachannel = event => {
             dataChannel = event.channel;
+            setupDataChannel();
         }
-        setupDataChannel();
 
     }
 
@@ -208,11 +148,11 @@ function setupDataChannel() {
     dataChannel.onopen = checkDataChannelState;
     dataChannel.onclose = checkDataChannelState;
     dataChannel.onmessage = event => {
-        console.info(event.data)
         const jsonData = JSON.parse(event.data);
         if (jsonData.type === "chat-message") {
             insertMessageToDOM(jsonData, false);
-        } else {
+        }
+         else {
             processSubtitles(jsonData);
         }
     };
@@ -223,16 +163,23 @@ function checkDataChannelState() {
 }
 
 function processSubtitles(options) {
+    console.info(options)
+    const subtitles = template.content.querySelector('subtitles');
+    subtitles.innerText = options.content;
+
     //TO DO  
+    
 }
 
 function insertMessageToDOM(options, isFromMe) {
     const template = document.querySelector('template[data-template="message"]');
     const nameEl = template.content.querySelector('.message__name');
+    const time = template.content.querySelector('time');
     let name = isFromMe ? 'Вы' : 'Наставник' ;
     if (options.emoji || name) {
         nameEl.innerText = options.emoji + ' ' + name;
     }
+    time.innerText = `${options.time.getHours()} : ${options.time.getMinutes()}`
     console.info('form lodaed',options);
 
     template.content.querySelector('.message__bubble').innerText = options.content;
@@ -264,6 +211,7 @@ form.addEventListener('submit', () => {
             name,
             content: value,
             emoji,
+            time: new Date(),
         };
         dataChannel.send(JSON.stringify(data));
         insertMessageToDOM(data, true);

4a24aefff9aa291f96c89c9b7f739cb89eaa121c
Vladislav Markushin <info@thefund.io>
2018-11-18 06:49:44 +0500

    new script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,234 +1,279 @@
-
 // Generate random room name if needed
 if (!location.hash) {
-  location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
+    location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
 }
 const roomHash = location.hash.substring(1);
 const possibleEmojis = [
-  '🐀','🐁','🐭','🐹','🐂','🐃','🐄','🐮','🐅','🐆','🐯','🐇','🐐','🐑','🐏','🐴',
-  '🐎','🐱','🐈','🐰','🐓','🐔','🐤','🐣','🐥','🐦','🐧','🐘','🐩','🐕','🐷','🐖',
-  '🐗','🐫','🐪','🐶','🐺','🐻','🐨','🐼','🐵','🙈','🙉','🙊','🐒','🐉','🐲','🐊',
-  '🐍','🐢','🐸','🐋','🐳','🐬','🐙','🐟','🐠','🐡','🐚','🐌','🐛','🐜','🐝','🐞',
+    '🐀', '🐁', '🐭', '🐹', '🐂', '🐃', '🐄', '🐮', '🐅', '🐆', '🐯', '🐇', '🐐', '🐑', '🐏', '🐴',
+    '🐎', '🐱', '🐈', '🐰', '🐓', '🐔', '🐤', '🐣', '🐥', '🐦', '🐧', '🐘', '🐩', '🐕', '🐷', '🐖',
+    '🐗', '🐫', '🐪', '🐶', '🐺', '🐻', '🐨', '🐼', '🐵', '🙈', '🙉', '🙊', '🐒', '🐉', '🐲', '🐊',
+    '🐍', '🐢', '🐸', '🐋', '🐳', '🐬', '🐙', '🐟', '🐠', '🐡', '🐚', '🐌', '🐛', '🐜', '🐝', '🐞',
 ];
+
 function randomEmoji() {
-  var randomIndex = Math.floor(Math.random() * possibleEmojis.length);
-  return possibleEmojis[randomIndex];
+    var randomIndex = Math.floor(Math.random() * possibleEmojis.length);
+    return possibleEmojis[randomIndex];
 }
 
 const emoji = randomEmoji();
 const name = prompt("What's your name?");
-// TODO: Replace with your own channel ID
 const drone = new ScaleDrone('pMbakAnQeahIRBtF');
-// Room name needs to be prefixed with 'observable-'
 const roomName = 'observable-' + roomHash;
 const configuration = {
-  iceServers: [{url:'stun:stun01.sipphone.com'},
-{url:'stun:stun.ekiga.net'},
-{url:'stun:stun.fwdnet.net'},
-{url:'stun:stun.ideasip.com'},
-{url:'stun:stun.iptel.org'},
-{url:'stun:stun.rixtelecom.se'},
-{url:'stun:stun.schlund.de'},
-{url:'stun:stun.l.google.com:19302'},
-{url:'stun:stun1.l.google.com:19302'},
-{url:'stun:stun2.l.google.com:19302'},
-{url:'stun:stun3.l.google.com:19302'},
-{url:'stun:stun4.l.google.com:19302'},
-{url:'stun:stunserver.org'},
-{url:'stun:stun.softjoys.com'},
-{url:'stun:stun.voiparound.com'},
-{url:'stun:stun.voipbuster.com'},
-{url:'stun:stun.voipstunt.com'},
-{url:'stun:stun.voxgratia.org'},
-{url:'stun:stun.xten.com'},
-{
-	url: 'turn:numb.viagenie.ca',
-	credential: 'muazkh',
-	username: 'webrtc@live.com'
-},
-{
-	url: 'turn:192.158.29.39:3478?transport=udp',
-	credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-	username: '28224511:1379330808'
-},
-{
-	url: 'turn:192.158.29.39:3478?transport=tcp',
-	credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-	username: '28224511:1379330808'
-}]
+    iceServers: [{
+        url: 'stun:stun01.sipphone.com'
+    },
+        {
+            url: 'stun:stun.ekiga.net'
+        },
+        {
+            url: 'stun:stun.fwdnet.net'
+        },
+        {
+            url: 'stun:stun.ideasip.com'
+        },
+        {
+            url: 'stun:stun.iptel.org'
+        },
+        {
+            url: 'stun:stun.rixtelecom.se'
+        },
+        {
+            url: 'stun:stun.schlund.de'
+        },
+        {
+            url: 'stun:stun.l.google.com:19302'
+        },
+        {
+            url: 'stun:stun1.l.google.com:19302'
+        },
+        {
+            url: 'stun:stun2.l.google.com:19302'
+        },
+        {
+            url: 'stun:stun3.l.google.com:19302'
+        },
+        {
+            url: 'stun:stun4.l.google.com:19302'
+        },
+        {
+            url: 'stun:stunserver.org'
+        },
+        {
+            url: 'stun:stun.softjoys.com'
+        },
+        {
+            url: 'stun:stun.voiparound.com'
+        },
+        {
+            url: 'stun:stun.voipbuster.com'
+        },
+        {
+            url: 'stun:stun.voipstunt.com'
+        },
+        {
+            url: 'stun:stun.voxgratia.org'
+        },
+        {
+            url: 'stun:stun.xten.com'
+        },
+        {
+            url: 'turn:numb.viagenie.ca',
+            credential: 'muazkh',
+            username: 'webrtc@live.com'
+        },
+        {
+            url: 'turn:192.158.29.39:3478?transport=udp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808'
+        },
+        {
+            url: 'turn:192.158.29.39:3478?transport=tcp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808'
+        }
+    ]
 };
 let room;
 let pc;
 
 
 function onSuccess() {};
+
 function onError(error) {
-  console.error(error);
+    console.error(error);
 };
 
 drone.on('open', error => {
-  if (error) {
-    return console.error(error);
-  }
-  room = drone.subscribe(roomName);
-  room.on('open', error => {
     if (error) {
-      onError(error);
+        return console.error(error);
     }
-  });
-  // We're connected to the room and received an array of 'members'
-  // connected to the room (including us). Signaling server is ready.
-  room.on('members', members => {
-    console.log('MEMBERS', members);
-    // If we are the second user to connect to the room we will be creating the offer
-    const isOfferer = members.length === 2;
-    startWebRTC(isOfferer);
-  });
+    room = drone.subscribe(roomName);
+    room.on('open', error => {
+        if (error) {
+            onError(error);
+        }
+    });
+    room.on('members', members => {
+        console.log('MEMBERS', members);
+        const isOfferer = members.length === 2;
+        startWebRTC(isOfferer);
+    });
 });
 
-// Send signaling data via Scaledrone
 function sendMessage(message) {
-  drone.publish({
-    room: roomName,
-    message
-  });
+    drone.publish({
+        room: roomName,
+        message
+    });
 }
 
 function startWebRTC(isOfferer) {
-  pc = new RTCPeerConnection(configuration);
+    pc = new RTCPeerConnection(configuration);
 
-  // 'onicecandidate' notifies us whenever an ICE agent needs to deliver a
-  // message to the other peer through the signaling server
-  pc.onicecandidate = event => {
-    if (event.candidate) {
-      sendMessage({'candidate': event.candidate});
-    }
-  };
+    pc.onicecandidate = event => {
+        if (event.candidate) {
+            sendMessage({
+                'candidate': event.candidate
+            });
+        }
+    };
 
-  // If user is offerer let the 'negotiationneeded' event create the offer
-  
-  if (isOfferer) {
-    // If user is offerer let them create a negotiation offer and set up the data channel
-    pc.onnegotiationneeded = () => {
-      pc.createOffer(localDescCreated, error => console.error(error));
-    }
-    dataChannel = pc.createDataChannel('chat');
-    setupDataChannel();
-  } else {
-    // If user is not the offerer let wait for a data channel
-    pc.ondatachannel = event => {
-      dataChannel = event.channel;
-      setupDataChannel();
-    }
-  }
 
-  // When a remote stream arrives display it in the #remoteVideo element
-  pc.ontrack = event => {
-    const stream = event.streams[0];
-    if (!remoteVideo.srcObject || remoteVideo.srcObject.id !== stream.id) {
-      remoteVideo.srcObject = stream;
+    if (isOfferer) {
+        pc.onnegotiationneeded = () => {
+            pc.createOffer(localDescCreated, error => console.error(error));
+        }
+        dataChannel = pc.createDataChannel('chat');
+        setupDataChannel();
+    } else {
+        pc.ondatachannel = event => {
+            dataChannel = event.channel;
+            setupDataChannel();
+        }
     }
-  };
 
-  navigator.mediaDevices.getUserMedia({
-    audio: false,
-    video: true,
-  }).then(stream => {
-    // Display your local video in #localVideo element
-    localVideo.srcObject = stream;
-    // Add your stream to be sent to the conneting peer
-    stream.getTracks().forEach(track => pc.addTrack(track, stream));
-}, onError);
-startListentingToSignals();
+    pc.ontrack = event => {
+        const stream = event.streams[0];
+        if (!remoteVideo.srcObject || remoteVideo.srcObject.id !== stream.id) {
+            remoteVideo.srcObject = stream;
+        }
+    };
+
+    navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: true,
+    }).then(stream => {
+        localVideo.srcObject = stream;
+        stream.getTracks().forEach(track => pc.addTrack(track, stream));
+    }, onError);
+    startListentingToSignals();
 }
 
 function startListentingToSignals() {
-  // Listen to signaling data from Scaledrone
-  room.on('data', (message, client) => {
-    // Message was sent by us
-    if (client.id === drone.clientId) {
-      return;
-    }
-
-    if (message.sdp) {
-      // This is called after receiving an offer or answer from another peer
-      pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
-        // When receiving an offer lets answer it
-        if (pc.remoteDescription.type === 'offer') {
-          pc.createAnswer().then(localDescCreated).catch(onError);
+    room.on('data', (message, client) => {
+        if (client.id === drone.clientId) {
+            return;
         }
-      }, onError);
-    } else if (message.candidate) {
-      // Add the new ICE candidate to our connections remote description
-      pc.addIceCandidate(
-        new RTCIceCandidate(message.candidate), onSuccess, onError
-      );
-    }
-  });
+
+        if (message.sdp) {
+            pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
+                if (pc.remoteDescription.type === 'offer') {
+                    pc.createAnswer().then(localDescCreated).catch(onError);
+                }
+            }, onError);
+        } else if (message.candidate) {
+            pc.addIceCandidate(
+                new RTCIceCandidate(message.candidate), onSuccess, onError
+            );
+        }
+    });
 }
 
 function localDescCreated(desc) {
-  pc.setLocalDescription(
-    desc,
-    () => sendMessage({'sdp': pc.localDescription}),
-    onError
-  );
+    pc.setLocalDescription(
+        desc,
+        () => sendMessage({
+            'sdp': pc.localDescription
+        }),
+        onError
+    );
 }
+
 function setupDataChannel() {
-  checkDataChannelState();
-  dataChannel.onopen = checkDataChannelState;
-  dataChannel.onclose = checkDataChannelState;
-  dataChannel.onmessage = event =>
-    insertMessageToDOM(JSON.parse(event.data), false)
+    checkDataChannelState();
+    dataChannel.onopen = checkDataChannelState;
+    dataChannel.onclose = checkDataChannelState;
+    dataChannel.onmessage = event => {
+        const jsonData = JSON.parse(event.data);
+        console.log(jsonData.name);
+        if (jsonData.type === "chat-message") {
+            insertMessageToDOM(jsonData, false);
+        } else {
+            processSubtitles(jsonData);
+        }
+    };
 }
 
 function checkDataChannelState() {
-  console.log('WebRTC channel state is:', dataChannel.readyState);
+    console.log('WebRTC channel state is:', dataChannel.readyState);
+}
+
+function processSubtitles(options) {
+    //TO DO  
 }
 
 function insertMessageToDOM(options, isFromMe) {
-  const template = document.querySelector('template[data-template="message"]');
-  const nameEl = template.content.querySelector('.message__name');
-  if (options.emoji || options.name) {
-    nameEl.innerText = options.emoji + ' ' + options.name;
-  }
-  template.content.querySelector('.message__bubble').innerText = options.content;
-  const clone = document.importNode(template.content, true);
-  const messageEl = clone.querySelector('.message');
-  if (isFromMe) {
-    messageEl.classList.add('message--mine');
-  } else {
-    messageEl.classList.add('message--theirs');
-  }
+    const template = document.querySelector('template[data-template="message"]');
+    const nameEl = template.content.querySelector('.message__name');
+    if (options.emoji || options.name) {
+        nameEl.innerText = options.emoji + ' ' + options.name;
+    }
+    template.content.querySelector('.message__bubble').innerText = options.content;
+    const clone = document.importNode(template.content, true);
+    const messageEl = clone.querySelector('.message');
+    if (isFromMe) {
+        messageEl.classList.add('message--mine');
+    } else {
+        messageEl.classList.add('message--theirs');
+    }
 
-  const messagesEl = document.querySelector('.messages');
-  messagesEl.appendChild(clone);
+    const messagesEl = document.querySelector('.messages');
+    messagesEl.appendChild(clone);
 
-  // Scroll to bottom
-  messagesEl.scrollTop = messagesEl.scrollHeight - messagesEl.clientHeight;
+    // Scroll to bottom
+    messagesEl.scrollTop = messagesEl.scrollHeight - messagesEl.clientHeight;
 }
 
-    
+
 
 
 const form = document.getElementById('form_test');
 console.log(form);
 
 form.addEventListener('submit', () => {
-  if (dataChannel.readyState === 'open') {
-      const input = document.querySelector('input[type="text"]');
-      const value = input.value;
-      input.value = '';
+    if (dataChannel.readyState === 'open') {
+        const input = document.querySelector('input[type="text"]');
+        const value = input.value;
+        input.value = '';
 
-      const data = {
-        name,
-        content: value,
-        emoji,
-      };
+        const data = {
+            type: "chat-message",
+            name,
+            content: value,
+            emoji,
+        };
 
-      dataChannel.send(JSON.stringify(data));
+        dataChannel.send(JSON.stringify(data));
 
-      insertMessageToDOM(data, true);
-  }
+        insertMessageToDOM(data, true);
+    }
 });
 
+function sendSubtitles(value) {
+    const data = {
+        type: "subtitles",
+        content: value,
+    };
+    dataChannel.send(JSON.stringify(data));
+}
